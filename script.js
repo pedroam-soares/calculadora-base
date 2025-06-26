c32c03d7b666ab680ee4f5550b63a8af546a08da
Pedro Soares <ps808686@gmail.com>
2025-06-26 09:14:43 -0300

    calculadora funcionado

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ console.log(inputNum2);
 console.log(operacoes);
 console.log(btnCalcular);
 console.log(resultado);
+function calcular() {
+  let resultadoDaOperacao;
+
+  if (operacoes.value === "+") {
+    resultadoDaOperacao = Number(inputNum1.value) + Number(inputNum2.value);
+  }
+  if (operacoes.value === "-") {
+    resultadoDaOperacao = Number(inputNum1.value) - Number(inputNum2.value);
+  }
+  if (operacoes.value === "*") {
+    resultadoDaOperacao = Number(inputNum1.value) * Number(inputNum2.value);
+  }
+  if (operacoes.value === "/") {
+    resultadoDaOperacao = Number(inputNum1.value) / Number(inputNum2.value);
+  }
+
+  resultado.textContent = `O resultado é ${resultadoDaOperacao}`;
+}
